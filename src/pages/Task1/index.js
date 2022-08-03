e3a8db67ba6c1397c9ca4975e3d318e86534bdec
Himanshu Kumar <himanshu064@gmail.com>
2022-08-03 13:06:19 +0530

    Setup initial app

---
+++ src/pages/Task1/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Task1 = () => {
+  return <div>Task1</div>;
+};
+
+export default Task1;
---
+++ src/pages/Task3/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Task3 = () => {
+  return <div>Task3</div>;
+};
+
+export default Task3;
