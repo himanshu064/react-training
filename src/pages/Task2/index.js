c6bb2cf61d4ac37510db7d7a174f321326ce3bf7
rahuls-netweb <rahuls.netweb@gmail.com>
2022-08-03 16:00:48 +0530

    task 2 done

--- src/pages/Task2/index.js
+++ src/pages/Task2/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 
-const Task2 = () => {
-  return <div>Task2</div>;
+const Task2 = (props) => {
+  var getTable = parseInt(props.table);
+  var table =[];
+  for (let i = 1; i <= 10; i++) {
+    table.push(<p>{getTable} x {i}  = {getTable * i}</p>) 
+  }
+  return table;
 };
 
 export default Task2;
