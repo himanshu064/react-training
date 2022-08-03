2814d6e55d6e2337dfdef54954126a47656a733e
rahuls-netweb <rahuls.netweb@gmail.com>
2022-08-03 15:24:05 +0530

    task 1 and task 2 done

--- src/pages/Task1/index.js
+++ src/pages/Task1/index.js
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Task1 = () => {
-  return <div>Task1</div>;
+  let obj = [
+    { id: Math.random(), text: "Walk the dog", textColor: "red" },
+    { id: Math.random(), text: "Prepare lunch", textColor: "green" },
+    { id: Math.random(), text: "Fix the bugs", textColor: "yellow" },
+    { id: Math.random(), text: "Code all day", textColor: "blue" },
+    { id: Math.random(), text: "Sleep", textColor: "black" },
+  ];
+  return (
+    <>
+      {obj.map(function (val) {
+        return (
+          <div key={val.id}>
+            <h1 style={{ color: `${val.textColor}` }}>{val.text}</h1>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default Task1;
+
--- src/pages/Task2/index.js
+++ src/pages/Task2/index.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-const Task2 = () => {
-  return <div>Task2</div>;
+const Task2 = (props) => {
+  let t = parseInt(props.table)
+ 
+  let table = []; 
+  for (let i = 1; i <= 10; i++) {
+    table.push( <p key={i}>{t} x {i} = {t * i}</p>);   
+  }
+  return table;
 };
 
 export default Task2;
