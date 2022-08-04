4824dc3fe76c87a22060e4904a1f8c7b8ffe4f5b
abhishek <abhisasa2001@gmail.com>
2022-08-04 19:02:36 +0530

    Task2

--- src/pages/Task2/index.js
+++ src/pages/Task2/index.js
@@ -1,7 +1,44 @@
-import React from "react";
+import { useState } from "react";
+import './index.css'
 
-const Task2 = () => {
-  return <div>Task2</div>;
-};
+export default function Table() {
 
-export default Task2;
+  const [multiple, setMultiple] = useState(3);
+
+  function onMultipleChanged(events) {
+    setMultiple(events.target.value);
+  }
+
+  return (
+    <>
+      <div className="App">
+        <h1 className="header">Multiplication Tables</h1>
+        <input type="number" className="num" value={multiple} onChange={onMultipleChanged} />
+<Num mul={multiple}/>
+      </div>
+    </>
+  );
+}
+const Num=({mul})=>{
+  const times = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return(
+    <>
+    <ul>
+  {times.map((t) => {
+    return (
+      
+    <div className="result">
+
+      <li className="result" key={t}>
+      {mul} X {t}  = {t * mul}
+      </li>
+    </div>
+
+
+    );
+  })}
+</ul>
+    </>
+  )
+  
+}
