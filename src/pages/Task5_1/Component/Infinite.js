464e02cba89709b077c4c75d2c52cc503477fb3a
unknown <mohitramola98@gmail.com>
2022-08-16 11:40:10 +0530

    fixe accordion issue in task 5 and task5_1

--- src/pages/Task5_1/Component/Infinite.js
+++ src/pages/Task5_1/Component/Infinite.js
@@ -27,7 +27,7 @@ const Infinite = () => {
     <>
       <h1 className="text-center pt-4">Comment task :- Infinite scrollable</h1>
       <section className="container mt-4">
-        <div class="accordion" id="accordionExample">
+        <div class="accordion accordion-flush" id="accordionFlushExample">
           <InfiniteScroll
             dataLength={comment.length}
             next={fetchMoreData}
@@ -38,32 +38,27 @@ const Infinite = () => {
               const { body, id, user } = item;
               const { username } = user;
               return (
-                <div
-                  class="accordion accordion-flush"
-                  id="accordionFlushExample"
-                >
-                  <div class="accordion-item mt-4">
-                    <h2 class="accordion-header" id={`flush-heading${id}`}>
-                      <button
-                        class="accordion-button collapsed"
-                        type="button"
-                        data-bs-toggle="collapse"
-                        data-bs-target={`#flush-collapse${id}`}
-                        aria-expanded="false"
-                        aria-controls={`flush-collapse${id}`}
-                      >
-                        {username}
-                      </button>
-                    </h2>
-                    <div
-                      id={`flush-collapse${id}`}
-                      class="accordion-collapse collapse"
-                      aria-labelledby={`flush-heading${id}`}
-                      data-bs-parent="#accordionFlushExample"
+                <div class="accordion-item mt-4">
+                  <h2 class="accordion-header" id={`flush-heading${id}`}>
+                    <button
+                      class="accordion-button collapsed"
+                      type="button"
+                      data-bs-toggle="collapse"
+                      data-bs-target={`#flush-collapse${id}`}
+                      aria-expanded="false"
+                      aria-controls={`flush-collapse${id}`}
                     >
-                      <div class="accordion-body">
-                        <p>{body}</p>
-                      </div>
+                      {username}
+                    </button>
+                  </h2>
+                  <div
+                    id={`flush-collapse${id}`}
+                    class="accordion-collapse collapse"
+                    aria-labelledby={`flush-heading${id}`}
+                    data-bs-parent="#accordionFlushExample"
+                  >
+                    <div class="accordion-body">
+                      <p>{body}</p>
                     </div>
                   </div>
                 </div>
