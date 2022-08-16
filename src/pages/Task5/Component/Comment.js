7668432eb10b52c232e567d0e87fb7975fd9ebf5
unknown <mohitramola98@gmail.com>
2022-08-16 12:48:35 +0530

    showing comment of each post

--- src/pages/Task5/Component/Comment.js
+++ src/pages/Task5/Component/Comment.js
@@ -3,14 +3,15 @@ import "../index.css";
 const Comment = () => {
   const [limit, setLimit] = useState(10);
   const [skip, setSkip] = useState(0);
-  const [comment, setComment] = useState([]);
+  const [post, setPost] = useState([]);
   const [jump, setJump] = useState(0);
+  const [postComment, setPostComment] = useState([]);
   const perPage = [0, 15, 25, 50, 70, 100];
-  let url = `https://dummyjson.com/comments?limit=${limit}&skip=${skip}`;
-  const fetchComment = async () => {
+  let url = `https://dummyjson.com/posts?limit=${limit}&skip=${skip}`;
+  const fetchPost = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    setComment(data.comments);
+    setPost(data.posts);
   };
   const HandleChange = (event) => {
     const LIMIT = parseInt(event.target.value);
@@ -18,8 +19,14 @@ const Comment = () => {
     setLimit(newLimit);
     setJump(event.target.value);
   };
+  const HandleComment = async (id) => {
+    const postUrl = `https://dummyjson.com/posts/${id}/comments`;
+    const response = await fetch(postUrl);
+    const data = await response.json();
+    setPostComment(data.comments);
+  };
   useEffect(() => {
-    fetchComment();
+    fetchPost();
   }, [skip, limit]);
 
   return (
@@ -27,9 +34,8 @@ const Comment = () => {
       <h1 className="text-center pt-4">Comment task</h1>
       <section className="container mt-4">
         <div class="accordion accordion-flush" id="accordionFlushExample">
-          {comment.map((item) => {
-            const { body, id, user } = item;
-            const { username } = user;
+          {post.map((item) => {
+            const { body, id, title } = item;
             return (
               <div class="accordion-item mt-4">
                 <h2 class="accordion-header" id={`flush-heading${id}`}>
@@ -40,8 +46,9 @@ const Comment = () => {
                     data-bs-target={`#flush-collapse${id}`}
                     aria-expanded="false"
                     aria-controls={`flush-collapse${id}`}
+                    onClick={() => HandleComment(id)}
                   >
-                    {username}
+                    {title}
                   </button>
                 </h2>
                 <div
@@ -51,7 +58,23 @@ const Comment = () => {
                   data-bs-parent="#accordionFlushExample"
                 >
                   <div class="accordion-body">
-                    <p>{body}</p>
+                    <span className="Post-body"> Post :-</span> <p>{body}</p>
+                    <h1 className="text-center" style={{ fontSize: "1.7rem" }}>
+                      Comment's
+                    </h1>
+                    {postComment.map((item) => {
+                      const { body, user, id } = item;
+                      const { username } = user;
+                      return (
+                        <div
+                          className="d-flex justify-content-between align-items-center comment"
+                          key={id}
+                        >
+                          <h1 style={{ color: "#ff7043" }}>{username}</h1>
+                          <h2 style={{ color: "#66bb6a" }}>{body}</h2>
+                        </div>
+                      );
+                    })}
                   </div>
                 </div>
               </div>
--- src/pages/Task5_1/Component/Infinite.js
+++ src/pages/Task5_1/Component/Infinite.js
@@ -4,24 +4,31 @@ import "../index.css";
 const Infinite = () => {
   const [limit, setLimit] = useState(10);
   const [skip, setSkip] = useState(0);
-  const [comment, setComment] = useState([]);
-  let url = `https://dummyjson.com/comments?limit=${limit}&skip=${skip}`;
+  const [post, setPost] = useState([]);
+  const [postComment, setPostComment] = useState([]);
+  let url = `https://dummyjson.com/posts?limit=${limit}&skip=${skip}`;
   const fetchComment = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    setComment(data.comments);
+    setPost(data.posts);
   };
   useEffect(() => {
     fetchComment();
   }, [skip]);
   const fetchMoreData = async () => {
     let newLimmit = 10 + limit;
-    const newUrl = `https://dummyjson.com/comments?limit=${newLimmit}`;
+    const newUrl = `https://dummyjson.com/posts?limit=${newLimmit}`;
     console.log(newUrl);
     const response = await fetch(newUrl);
     const data = await response.json();
-    setComment(data.comments);
+    setPost(data.posts);
     setLimit(newLimmit);
+  };
+  const HandleComment = async (id) => {
+    const postUrl = `https://dummyjson.com/posts/${id}/comments`;
+    const response = await fetch(postUrl);
+    const data = await response.json();
+    setPostComment(data.comments);
   };
   return (
     <>
@@ -29,14 +36,13 @@ const Infinite = () => {
       <section className="container mt-4">
         <div class="accordion accordion-flush" id="accordionFlushExample">
           <InfiniteScroll
-            dataLength={comment.length}
+            dataLength={post.length}
             next={fetchMoreData}
-            hasMore={comment.length < 340 ? true : false}
+            hasMore={post.length < 340 ? true : false}
             loader={<h4>Loading...</h4>}
           >
-            {comment.map((item) => {
-              const { body, id, user } = item;
-              const { username } = user;
+            {post.map((item) => {
+              const { body, title, id } = item;
               return (
                 <div class="accordion-item mt-4">
                   <h2 class="accordion-header" id={`flush-heading${id}`}>
@@ -47,8 +53,9 @@ const Infinite = () => {
                       data-bs-target={`#flush-collapse${id}`}
                       aria-expanded="false"
                       aria-controls={`flush-collapse${id}`}
+                      onClick={() => HandleComment(id)}
                     >
-                      {username}
+                      {title}
                     </button>
                   </h2>
                   <div
@@ -58,7 +65,27 @@ const Infinite = () => {
                     data-bs-parent="#accordionFlushExample"
                   >
                     <div class="accordion-body">
+                      <span className="Post-body pb-3">Post :- </span>
                       <p>{body}</p>
+                      <h1
+                        className="text-center"
+                        style={{ fontSize: "1.7rem" }}
+                      >
+                        Comment's
+                      </h1>
+                      {postComment.map((item) => {
+                        const { body, user, id } = item;
+                        const { username } = user;
+                        return (
+                          <div
+                            className="d-flex justify-content-between align-items-center comment"
+                            key={id}
+                          >
+                            <h1 style={{ color: "#ff7043" }}>{username}</h1>
+                            <h2 style={{ color: "#66bb6a" }}>{body}</h2>
+                          </div>
+                        );
+                      })}
                     </div>
                   </div>
                 </div>
