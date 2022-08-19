0840505c4bf2daccdd26b855963fb4a8d4f4d792
Harmanjot Singh <harmanjot.netweb@gmail.com>
2022-08-19 12:01:43 +0530

    Task 5 using infinite scrollBar - Done

---
+++ src/pages/Task5_1/index.js
@@ -0,0 +1,16 @@
+import React, { useState, } from "react";
+import AccordionComponent from "./Accordion";
+import "./rc-select.css"
+
+
+function Pagination() {
+    const [postsperpage] = useState(20)
+
+    return <div>
+        <h1>Data Using Infinite Scrollbar</h1>
+        <AccordionComponent postsperpage={postsperpage}/>
+    </div>
+}
+
+
+export default Pagination;
