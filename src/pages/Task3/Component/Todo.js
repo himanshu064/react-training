bb8fba52c7c48f4a9b88c28c59b8ce8ea3242741
abhishek <abhisasa2001@gmail.com>
2022-08-23 16:57:32 +0530

    task3

--- src/pages/Task3/Component/Todo.js
+++ src/pages/Task3/Component/Todo.js
@@ -1,73 +1,60 @@
-import React from "react";
-import "../index.css";
+import react from "react";
+import "../Todo.css";
 import TodoList from "./TodoList";
 import { useGlobalContext } from "../../Context";
 
-const Todo = () => {
-  const {
-    task,
-    HandleChange,
-    HandleClick,
-    AllTask,
-    HandleActive,
-    HandleCompleted,
-    ShowAll,
-    HandleAllTask,
-    checked,
-  } = useGlobalContext();
+const Todo = ()=>{
+    const {
+        task,
+        allTask,
+        check,
+        HandleChange,
+        HandleClick,
+        HandleActive,
+        HandleComplete,
+        ShowAll,
+        HandleAllTask
 
-  return (
+    } = useGlobalContext();
+    return(
     <>
-      <div className="todo-bg d-flex justify-content-center align-items-center flex-column">
+    <div>
+        <h3 className="head">Welcome In Todo List...</h3>
+    </div>
+    <div className="container">
+    <div className="todo-bg d flex justify-content-center align-items-center flex-column">
         <div className="todo-container p-3">
-          <div className="mb-3">
-            <div className="d-flex justify-content-center  AddTask">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="what needs to be done"
-                value={task}
-                onChange={HandleChange}
-                onKeyPress={HandleClick}
-              />
+            <div className="mb-3">
+                <div className="d-flex justify-content-center  AddTask">
+                    <input type="text" className="form-control" placeholder="What needs to be done" value={task} onChange={HandleChange} onKeyPress={HandleClick} />
+                </div>
+                <div className="todo-task p-4">
+                {allTask.length ===0 ? (
+                    <h4 className="enter-task"> Please Enter Your Task...</h4>
+                ) : (
+                    <TodoList />
+                )}
+                </div>
+                <hr />
+                <div className="d-flex justify-content-between align-items-center  CheckAll">
+                    <div className="form-check">
+                        <input className="form-check-input" type="checkbox" check={check} onChange={() =>HandleAllTask()} />
+                        <label className="form-check-label">Check All</label>
+                    </div>
+                    <h4 className="count-task">No task....</h4>
+                </div>
+                <hr />
+                <div className="task-btn">
+                    <button className="btn1" onClick={ShowAll}>All</button>
+                    <button className="btn1" onClick={HandleActive}>Active</button>
+                    <button className="btn1" onClick={HandleComplete}>Completed</button>
+                </div>
             </div>
-
-            <div className="todo-task p-4">
-              {AllTask.length === 0 ? (
-                <h1 className="enter-task">Plese enter task !!!!!</h1>
-              ) : (
-                <TodoList />
-              )}
-            </div>
-            <hr />
-            <div className="d-flex justify-content-between align-items-center checkAll">
-              <div className="form-check">
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  checked={checked}
-                  onChange={() => HandleAllTask()}
-                />
-                <label className="form-check-label">Check All</label>
-              </div>
-              <h1 className="count-task">No task....</h1>
-            </div>
-            <hr />
-            <div className="task-btn">
-              <button className="btn" onClick={ShowAll}>
-                All
-              </button>
-              <button className="btn" onClick={HandleActive}>
-                Active
-              </button>
-              <button className="btn" onClick={HandleCompleted}>
-                Completed
-              </button>
-            </div>
-          </div>
         </div>
-      </div>
+    </div>
+    </div>
     </>
-  );
+    );
 };
+
 export default Todo;
