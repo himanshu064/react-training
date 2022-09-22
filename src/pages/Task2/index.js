b10cc0b6f41d2b50daf90d2bca1d5f4489585419
Harmanjot Singh <harmanjot.netweb@gmail.com>
2022-09-22 10:31:51 +0530

    Task  - 2

--- src/pages/Task2/index.js
+++ src/pages/Task2/index.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Task2 = () => {
-  return <div>Task2</div>;
-};
+function TableForAny(props) {
+  const number = props.number;
+  var length = props.max;
+  length = parseInt(length);
 
-export default Task2;
+  function range(length) {
+    if (length === undefined) {
+      return Array(10)
+        .fill()
+        .map((_, idx) => idx + 1);
+    } else {
+      return Array(length)
+        .fill()
+        .map((_, idx) => idx + 1);
+    }
+  }
+  var result = range(length);
+  const [times] = useState(result);
+
+  return (
+    <div>
+      {times.map((x, index) => {
+        console.log(x);
+        return (
+          <p key={index}>
+            {number} X {x} = {number * x}
+          </p>
+        );
+      })}
+    </div>
+  );
+}
+
+export default TableForAny;
