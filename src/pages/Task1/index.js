ecc40f167b144503f0afeb030305df51f7a29940
Harmanjot Singh <harmanjot.netweb@gmail.com>
2022-09-22 10:01:12 +0530

    Task - 1

--- src/pages/Task1/index.js
+++ src/pages/Task1/index.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Task1 = () => {
-  return <div>Task1</div>;
-};
+function ColorChange() {
+  const [allobject] = useState([
+    { text: "Hello", color: "red" },
+    { text: "Hi", color: "green" },
+    { text: "Tata", color: "yellow" },
+    { text: "Bye", color: "blue" },
+    { text: "Good", color: "purple" },
+    { text: "Bad", color: "grey" },
+    { text: "Easy", color: "black" },
+    { text: "Tasty", color: "violet" },
+  ]);
 
-export default Task1;
+  return (
+    <div>
+      {allobject.map((item) => {
+        console.log("Text : " + item.text);
+        console.log("Color : " + item.color);
+        return (
+          <h2 style={{ color: item.color, textAlign: "center" }}>
+            {item.text}
+          </h2>
+        );
+      })}
+    </div>
+  );
+}
+
+export default ColorChange;
