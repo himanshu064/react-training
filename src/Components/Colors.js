02f90f63995f5563018027740399f611c1bad202
devMohit98 <mohitramola98@gmail.com>
2022-10-03 18:53:35 +0530

    assignment 5

---
+++ src/Components/Colors.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./style.css";
+const Colors = () => {
+  const [palette, setPalette] = useState([
+    "#ECA400",
+    "#EAF8BF",
+    "#006992",
+    "#27476E",
+    "#001D4A",
+  ]);
+  const generateMore = () => {
+    let addPalette = [];
+    for (let i = 0; i < 5; i++) {
+      let random = "#" + Math.floor(Math.random() * 16777215).toString(16);
+      addPalette.push(random);
+    }
+    setPalette(addPalette);
+
+    let copyToClipBoard = JSON.stringify(Object.assign({}, addPalette));
+    navigator.clipboard.writeText(copyToClipBoard);
+  };
+  document.body.onkeyup = (event) => {
+    if (event.code === "Space") {
+      generateMore();
+    }
+  };
+
+  return (
+    <>
+      <h1 className="heading mt-3 mb-3">Color palette generator</h1>
+      <div className="container mt-3">
+        <div className="row">
+          {palette.map((item, index) => {
+            return (
+              <div key={index} className="col">
+                <div className="box">
+                  <div className="color" style={{ background: item }}></div>
+                  <div className="card-item">{item}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <button className="btn btn-primary" onClick={generateMore}>
+            generator more palette
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default Colors;
