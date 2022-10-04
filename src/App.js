b9c68407121f553b1cd1b164c2497ea4a9e81c6c
abhishek <abhisasa2001@gmail.com>
2022-10-04 16:21:22 +0530

    assignment6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import Shortner from './Assign6/Shortner';
+import History from './Assign6/History';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Shortner/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Shortner/>} ></Route>
+        <Route path='/history' element={<History/>} ></Route>
+      </Routes>
+    </BrowserRouter>
     </>
   );
 }
--- src/Assign6/Shortner.js
+++ src/Assign6/Shortner.js
@@ -1,10 +1,75 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import './style.css';
 
 const Shortner = () => {
+    const getVisited = () => {
+        if (JSON.parse(localStorage.getItem("links")) === null) {
+          return [];
+        } else {
+          return JSON.parse(localStorage.getItem("links"));
+        }
+      };
+    const shortenLinks = () => {
+        if (JSON.parse(localStorage.getItem("shortenLinks")) === null) {
+          return [];
+        } else {
+          return JSON.parse(localStorage.getItem("shortenLinks"));
+        }
+      };
+    const [input, setInput] = useState("");
+    const [shortenedLink, setShortenedLink] = useState(shortenLinks());
+    const [history,setHistory] = useState (getVisited());
+    
+    const HandleChange = (e) =>{
+        setInput(e.target.value)
+    }
+    const fetchData = async () => {
+        try {
+            const response = await axios(
+                `https://api.shrtco.de/v2/shorten?url=${input}`
+                );
+                setShortenedLink(response.data.result.full_short_link);
+                let data = [...history,response.data.result.full_short_link];
+                setHistory(data);    
+        } catch (e) {
+          console.log(e);
+        }
+    };
+    const HandleShortUrl = () =>{
+          fetchData();
+    }
+    const HandleCopy = () =>{
+        navigator.clipboard.writeText(shortenedLink); 
+    }
+    localStorage.setItem("links",JSON.stringify(history));
+    localStorage.setItem("shortenLinks",JSON.stringify(shortenedLink));
+
   return (
-    <div>
-      
+    <>
+    <div className="header">
+        <h2>Shortener</h2>
+        <Link to='/history'>
+        <button className='history'>History</button>
+        </Link>
     </div>
+    <div className="body">
+        <h1>URL Shortner</h1>
+        <p>Shortner is free tool to shorten URLs. Short, memorable link in seconds</p>
+        <input className='text' onChange={HandleChange} value={input} type="text" placeholder='Enter link here' />
+        <button className='btn' onClick={HandleShortUrl}>Short URL</button>
+        {shortenedLink.length > 1 ? (
+            <>
+            <h2 className='link'>Your Shorten Link</h2>
+            <div className='footer'>{shortenedLink}
+            <i onClick={HandleCopy} className="fa-solid fa-copy"></i>
+            </div>
+            </>
+         ):('')     
+        }
+    </div>
+    </>
   )
 }
 
