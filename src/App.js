92a31e9bbb458aa228f8795f8b47e9683e6da2fb
devMohit98 <mohitramola98@gmail.com>
2022-10-04 10:39:07 +0530

    assign 6

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Task1 from "./pages/Task1";
-import Task2 from "./pages/Task2";
-import Task3 from "./pages/Task3";
-
-function App() {
+import React from "react";
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/task1' element={<Task1 />} />
-        <Route path='/task2' element={<Task2 />} />
-        <Route path='/task3' element={<Task3 />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <h1>Shorten url website</h1>
+    </>
   );
-}
-
+};
 export default App;
---
+++ src/Component/ShortenUrl.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShortenUrl = () => {
+  return <div></div>;
+};
+
+export default ShortenUrl;
