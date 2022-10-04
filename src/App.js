ab5ba1c8373bad8cc7f74b24c895c40ef2f13cb7
devMohit98 <mohitramola98@gmail.com>
2022-10-04 16:11:11 +0530

    assignment 7

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Task1 from "./pages/Task1";
-import Task2 from "./pages/Task2";
-import Task3 from "./pages/Task3";
+import React from "react";
 
-function App() {
+const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/task1' element={<Task1 />} />
-        <Route path='/task2' element={<Task2 />} />
-        <Route path='/task3' element={<Task3 />} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <h1>typing game</h1>
+    </div>
   );
-}
+};
 
 export default App;
