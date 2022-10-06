85f4c53c7bdcb01de43fbc3d62e920b63264d92a
Harmanjot Singh <harmanjot.netweb@gmail.com>
2022-10-06 11:41:53 +0530

    Assignment - 6 (Link Shortner)

---
+++ src/pages/Assignment6/PopUp.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import styles from "./index.module.css";
+
+function PopUp(props) {
+  const openInNewTab = (shortcode) => {
+    window.open(shortcode, "_blank", "noopener noreferrer");
+  };
+  const copyToClipboard = async (shortcode) => {
+    return await navigator.clipboard.writeText(shortcode);
+  };
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      {console.log(
+        props.data.map((item) => {
+          return (
+            <div className={styles.outputText}>
+              <a
+                href={item.shortcode}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={(e) => {
+                  openInNewTab(item.shortcode);
+                }}
+              >
+                {item.shortcode}
+              </a>
+            </div>
+          );
+        })
+      )}
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">History</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        {props.data.map((item) => {
+          return (
+            <div className={styles.outputDiv} style={{ marginBottom: "5px" }}>
+              <div className={styles.outputText}>
+                <a
+                  href={item.shortcode}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={(e) => {
+                    openInNewTab(item.shortcode);
+                  }}
+                >
+                  {item.shortcode}
+                </a>
+                <p className={styles.para}>Full Url : {item.fullURL}</p>
+              </div>
+              <Button
+                variant="outline-secondary"
+                id="button-addon2"
+                onClick={(e) => {
+                  copyToClipboard(item.shortcode);
+                }}
+                className={styles.outputBtn}
+              >
+                {" "}
+                Copy to clipboard
+              </Button>
+            </div>
+          );
+        })}
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+export default PopUp;
---
+++ src/pages/Assignment6/index.js
@@ -0,0 +1,151 @@
+import React, { useEffect, useState } from "react";
+import { Button, Form, InputGroup } from "react-bootstrap";
+import styles from "./index.module.css";
+import PopUp from "./PopUp";
+
+const Assignment6 = () => {
+  const [shortenedLink, setShortenedLink] = useState("");
+  const [userInput, setUserInput] = useState("");
+  const [modalShow, setModalShow] = useState(false);
+
+  var codes;
+  let storage = localStorage.getItem("shortcodes");
+  if (storage === null) {
+    codes = [];
+  } else {
+    codes = JSON.parse(storage);
+  }
+  useEffect(() => {
+    updateData();
+  }, [shortenedLink]);
+
+  const [data, setData] = useState(codes);
+  const makeitShort = () => {
+    if (userInput !== "") {
+      let shortlink = makeRandom();
+      const newdata = [...data];
+      newdata.map((item) => {
+        if (item.fullURL === userInput) {
+          shortlink = item.shortcode;
+        }
+      });
+      let response;
+      if (shortlink.includes("https://hrmn/")) {
+        response = shortlink;
+      } else {
+        response = `https://hrmn/${shortlink}`;
+      }
+      setShortenedLink(response);
+    }
+  };
+  function makeRandom() {
+    var result = "";
+    var characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var charactersLength = characters.length;
+    for (var i = 0; i < 6; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+  const copyToClipboard = async () => {
+    return await navigator.clipboard.writeText(shortenedLink);
+  };
+  const openInNewTab = (code) => {
+    const link = openUrl(code);
+    window.open(link, "_blank", "noopener noreferrer");
+  };
+  const openUrl = (code) => {
+    const newdata = [...data];
+    let response;
+    newdata.map((item) => {
+      if (item.shortcode === code) {
+        return (response = item.fullURL);
+      }
+    });
+    return response;
+  };
+  const updateData = () => {
+    const finder = data.find((obj) => {
+      return obj.fullURL === userInput;
+    });
+    if (finder === undefined) {
+      if (userInput) {
+        const newData = [
+          ...data,
+          {
+            shortcode: shortenedLink,
+            fullURL: userInput,
+          },
+        ];
+        setData(newData);
+        localStorage.setItem("shortcodes", JSON.stringify(newData));
+      }
+    }
+  };
+  return (
+    <div className={styles.mainDiv}>
+      <div className={styles.historyBtn}>
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          History
+        </Button>
+        <PopUp
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+          data={data}
+        />
+      </div>
+      <div className={styles.mainText}>URL Shortner</div>
+      <div>
+        <InputGroup className={styles.inputFields}>
+          <Form.Control
+            placeholder="Enter Your Link Here"
+            aria-label="Your Link"
+            aria-describedby="basic-addon2"
+            value={userInput}
+            onChange={(e) => {
+              setUserInput(e.target.value);
+            }}
+          />
+          <Button
+            variant="outline-secondary"
+            id="button-addon2"
+            onClick={() => {
+              makeitShort();
+            }}
+          >
+            Short Url
+          </Button>
+        </InputGroup>
+      </div>
+      <div className={styles.textDiv}>Your Shortner Link </div>
+      <div className={styles.outputDiv}>
+        <div className={styles.outputText}>
+          <a
+            href={openUrl(shortenedLink)}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              openInNewTab(shortenedLink);
+            }}
+          >
+            {shortenedLink}
+          </a>
+        </div>
+        <Button
+          variant="outline-secondary"
+          id="button-addon2"
+          onClick={() => {
+            copyToClipboard();
+          }}
+          className={styles.outputBtn}
+        >
+          {" "}
+          Copy to clipboard
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Assignment6;
