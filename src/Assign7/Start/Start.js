d2de2d69486d23781bc21e8a9ce6bb738c0653d7
abhishek <abhisasa2001@gmail.com>
2022-10-07 19:03:12 +0530

    assignment7

--- src/Assign7/Start/Start.js
+++ src/Assign7/Start/Start.js
@@ -1,19 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './style.css';
 
 const Start = () => {
-  const timing =  [1,2,3,5,10];
+  const [timer, setTimer] = useState("1");
+
+  const HandleSetTimer = (e) => {
+    setTimer(e.target.value);
+    console.log(timer,'timer')
+  };
   return (
     <>
     <div className="container">
       <h1>Check your typing skills in a minute</h1>
-      <select id="select">
-        <option selected>Select Time</option>
-        {timing.map((time,index)=>{
-          return(
-              <option key={index} value={index + 1}>{time} Minute Test</option>
-          )
-        })}
+      <select id="select" value={timer}onChange={HandleSetTimer} >
+          <option value="1">1 minutes</option>
+          <option value="2">2 minutes</option>
+          <option value="3">3 minutes</option>
+          <option value="5">5 minutes</option>
+          <option value="10">10 minutes</option>       
       </select>
       <br />
       <button>START TEST</button>
