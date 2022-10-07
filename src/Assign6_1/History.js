7b0a231229f40e8d1f4f79d81d7c013ee468ca1a
abhishek <abhisasa2001@gmail.com>
2022-10-07 15:07:19 +0530

    assignment6_1

--- src/Assign6_1/History.js
+++ src/Assign6_1/History.js
@@ -3,15 +3,19 @@ import {Link} from 'react-router-dom';
 import './style.css';
 
 const History = () => {
-let history = JSON.parse(localStorage.getItem("links"));
+    const HandleCopy =(item) => {
+        // console.log(index,'index')
+     navigator.clipboard.writeText(item.shortcode);
+      };
+let history = JSON.parse(localStorage.getItem("BothUrl"));
 console.log(history,'a')
 return (
 <>
 <Link to='/'> <h2>Shortener</h2></Link>
 <h2 className='container'>Your History</h2>
-{history.map((item,index)=>{
+{history.map((item)=>{
 return(
-<div className="area">{item}</div>
+ <div className="area">{item.shortcode}  <i onClick={()=>HandleCopy(item)} className="fa-solid fa-copy"></i>      </div>
 )
 })}
 </>
--- src/Assign6_1/Shortner.js
+++ src/Assign6_1/Shortner.js
@@ -1,85 +1,128 @@
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import {Link} from 'react-router-dom';
 import { v4 as uuid } from "uuid";
-import './style.css';
 
 const Shortner = () => {
-const getVisited = () => {
-if (JSON.parse(localStorage.getItem("links")) === null) {
-return [];
-} else {
-return JSON.parse(localStorage.getItem("links"));
-}
-};
-const shortenLinks = () => {
-if (JSON.parse(localStorage.getItem("shortenLinks")) === null) {
-return [];
-} else {
-return JSON.parse(localStorage.getItem("shortenLinks"));
-}
-};
-const [input, setInput] = useState("");
-const [shortenedLink, setShortenedLink] = useState(shortenLinks());
-const [history,setHistory] = useState (getVisited());
-const HandleChange = (e) =>{
-setInput(e.target.value)
-}
-// const fetchData = async () => {
-// try {
-// const response = await axios(
-// `https://api.shrtco.de/v2/shorten?url=${input}`
-// );
-// setShortenedLink(response.data.result.full_short_link);
-// let data = [...history,response.data.result.full_short_link];
-// setHistory(data); 
-// } catch (e) {
-// console.log(e);
-// }
-// };
-let baseUrl = `https://Abhishek/`;
-const HandleShortUrl = () =>{
-const unique_id = uuid();
-const small_id = unique_id.slice(0, 6);
-let url = `${baseUrl}${small_id}`; 
-setShortenedLink(url);
-let data = [...history,url];
-setHistory(data); 
+    const [input, setInput] = useState("");
+    const [shortenedLink, setShortenedLink] = useState('');
 
-}
-const HandleCopy = () =>{
-navigator.clipboard.writeText(shortenedLink); 
-}
-const HandleActualURL = ()=>{
-window.location.href = input;
-}
-localStorage.setItem("links",JSON.stringify(history));
-localStorage.setItem("shortenLinks",JSON.stringify(shortenedLink));
+  var url;
+  let BothUrl = localStorage.getItem("BothUrl");
+  if (BothUrl === null) {
+    url = [];
+  } else {
+    url = JSON.parse(BothUrl);
+  }
+  const HandleChange = (e) =>{
+        setInput(e.target.value)
+    }
+  useEffect(() => {
+    updateData();
+    const url = window.location.href;
+    const [id, ...rest] = url.split("").reverse().join("").split("/");
+    if (id) {
+      const foundObject = history.find((item) => {
+        return item.shortUrl === `${window.location.href}`;
+      });
+      window.location.href = foundObject.fullURL;
+    }
+  }, [shortenedLink]);
 
-return (
-<>
-<div className="header">
-<h2>Shortener</h2>
-<Link to='/history'>
-<button className='history'>History</button>
-</Link>
-</div>
-<div className="body">
-<h1>URL Shortner</h1>
-<p>Shortner is free tool to shorten URLs. Short, memorable link in seconds</p>
-<input className='text' onChange={HandleChange} value={input} type="text" placeholder='Enter link here' />
-<button className='btn' onClick={HandleShortUrl}>Short URL</button>
-{shortenedLink.length > 1 ? (
-<>
-<h2 className='link'>Your Shorten Link</h2>
-<div className='footer'> <h3 onClick={HandleActualURL}> {shortenedLink}</h3> 
-<i onClick={HandleCopy} className="fa-solid fa-copy"></i>
-</div>
-</>
-):('') 
+  const [history, setHistory] = useState(url);
+  const ShortUrl = async (e) => {
+    if (input !== "") {
+      let shortlink = RandomString();
+      console.log(shortlink,'short')
+      const newdata = [...history];
+      newdata.map((item) => {
+        if (item.fullURL === input) {
+          shortlink = item.shortUrl;
+        }
+      });
+      let response;
+      if (shortlink.includes(window.location.href)) {
+        response = shortlink;
+      } else {
+        response = `${window.location.href}${shortlink}`;
+      }
+      setShortenedLink(response);
+    }
+  };
+  function RandomString() {
+    const unique_id = uuid();
+    const small_id = unique_id.slice(0, 6);
+    return small_id;
 }
-</div>
-</>
-)
+  const HandleCopy = async () => {
+    return await navigator.clipboard.writeText(shortenedLink);
+  };
+  const openInNewTab = (code) => {
+    const link = openUrl(code);
+    window.open(link, "_blank", "noopener noreferrer");
+  };
+  const openUrl = (code) => {
+    const newdata = [...history];
+    let response;
+    newdata.map((item) => {
+      if (item.shortUrl === code) {
+        return (response = item.fullURL);
+      }
+    });
+    return response;
+  };
+  const updateData = () => {
+    const finder = history.find((obj) => {
+      return obj.fullURL === input;
+    });
+    if (finder === undefined) {
+      if (input) {
+        const newData = [
+          ...history,
+          {
+            shortUrl: shortenedLink,
+            fullURL: input,
+          },
+        ];
+        setHistory(newData);
+        localStorage.setItem("BothUrl", JSON.stringify(newData));
+      }
+    }
+  };
+  return (
+    <>
+    <div className="header">
+        <h2>Shortener</h2>
+        <Link to='/history'>
+        <button className='history'>History</button>
+        </Link>
+    </div>
+    <div className="body">
+        <h1>URL Shortner</h1>
+        <p>Shortner is free tool to shorten URLs. Short, memorable link in seconds</p>
+        <input className='text' onChange={HandleChange} value={input} type="text" placeholder='Enter link here' />
+        <button className='btn' onClick={ShortUrl}>Short URL</button>
+        {shortenedLink.length > 1 ? (
+            <>
+            <h2 className='link'>Your Shorten Link</h2>
+            <div className='footer'>
+            <a
+            href={openUrl(shortenedLink)}
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={() => {
+              openInNewTab(shortenedLink);
+            }}
+          >
+            {shortenedLink}
+          </a>
+            <i onClick={HandleCopy} className="fa-solid fa-copy"></i>
+            </div>
+            </>
+          ):('')   
+         }
+    </div>
+    </>
+  )
 }
 
 export default Shortner;
